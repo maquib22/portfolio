666e70816f6305d6174700a377214d2d443ae1c0
Aaquib <maquib@dmspro.online>
2023-11-24 02:08:14 +0530

    hamburger menu

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+    document.querySelector('.sm-nav').classList.toggle('change')
+})
+document.querySelector('.nav-list').addEventListener('click', () => {
+    document.querySelector('.sm-nav').classList.toggle('change')
+})
